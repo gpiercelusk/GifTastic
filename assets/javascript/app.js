9e4ba7acbe88af92e657a999b0a9f2cf1eeab083
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-03-12 19:57:26 -0400

    almost done with logic still needs styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var topics = ["Alpacas", "Cooking", "Baking", "Arctic Monkyes", "The Beatles", "One Piece"];
+var topics = ["Alpacas", "Cooking", "Baking", "Arctic Monkyes", "The Beatles", "One Piece", "Dethklok", "Chocolate", "Schnauzer"];
 
 
 var APIkey = "bSjgDH5z6GZLhVQNHZLRlLNvnOhhG65r";
@@ -14,7 +14,7 @@ function renderButtons() {
     //sets "a" to generate a button
     var a = $("<button>");
     //adds a class of topic-btn to the button
-    a.addClass("topic-btn");
+    a.addClass("topic-btn btn btn-warning");
     //adds attribute to the buttons
     a.attr("data-name", topics[i]);
     //adds text to the button 
@@ -39,34 +39,42 @@ $("#add-gifs").on("click", function (event) {
 });
 
 function displayGIFS() {
-  $("button").on("click", function () {
-    var topicGIF = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      topicGIF + "&api_key=" + APIkey + "&limit=10";
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      .then(function (response) {
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
-          // if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+  var topicGIF = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    topicGIF + "&api_key=" + APIkey + "&limit=10";
+  $("#gifArea").empty();
+  //ajax call for button pressed
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+      console.log(response)
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        // keeps this SFW
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+          //creates new div for the gifs to go to 
           var gifDiv = $("<div>");
+          //shows the gifs rating and creates a p tag for it
           var rating = results[i].rating;
           var p = $("<p>").text("Rating: " + rating);
-          var gifImage = $("<img>");
-          gifImage.attr("src", results[i].images.fixed_height.url);
+          // creates a place for the gifimage to go
+          var gifImageStill = $("<img>");
+          gifImageStill.attr("src", results[i].images.original_still.url);
           gifDiv.append(p);
-          gifDiv.append(gifImage);
+          gifDiv.append(gifImageStill);
           $("#gifArea").prepend(gifDiv);
-          // }
         }
-      });
-  });
+      }
+    });
 };
+function clickToPlay() {
+  var state = $(this).attr("src", results[i].images.original_still.url);
+  console.log(state)
+}
+$(document).on("click", ".topic-btn", displayGIFS)
 
-// $(document).on("click", ".topic-btn", )
 
 //calls the renderButtons function to display initial topic array
 renderButtons();
