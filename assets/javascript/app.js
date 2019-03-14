8ba5e3baaf44871b64396fb4f86cb24751cde974
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-03-14 16:17:00 -0400

    logic done still fussing with how i want it styled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,23 @@
-var topics = ["Alpacas", "Cooking", "Baking", "Arctic Monkyes", "The Beatles", "One Piece", "Dethklok", "Chocolate", "Schnauzer"];
+//calls the renderButtons function to display initial topic array
+
+
+var topics = ["Alpacas", "Cooking", "Baking", "Arctic Monkyes", "The Beatles", "One Piece", "Dethklok", "Chocolate", "Schnauzer", "Vinyl", "Primus", "Ska"];
 
 
 var APIkey = "bSjgDH5z6GZLhVQNHZLRlLNvnOhhG65r";
-
+var gifCount = 10;
 
 
 // Function for displaying topics data
 function renderButtons() {
   //empties the button view div
   $("#buttons-view").empty();
-  //loops thought the array and creates a button for each topic
+  //loops through the array and creates a button for each topic
   for (var i = 0; i < topics.length; i++) {
     //sets "a" to generate a button
     var a = $("<button>");
     //adds a class of topic-btn to the button
-    a.addClass("topic-btn btn btn-warning");
+    a.addClass("topic-btn btn btn-warning col-xs-12 col-sm-6 col-md-3 col-lg-2");
     //adds attribute to the buttons
     a.attr("data-name", topics[i]);
     //adds text to the button 
@@ -23,6 +26,52 @@ function renderButtons() {
     $("#buttons-view").append(a);
   }
 };
+
+function displayGIFS() {
+  var topicGIF = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    topicGIF + "&api_key=" + APIkey + "&limit=" + gifCount;
+  //ajax call for button pressed
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+      console.log(queryURL)
+      console.log(response)
+      var results = response.data;
+      // loop the array of results
+      for (var i = 0; i < results.length; i++) {
+        // keeps this SFW
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+          //creates new div for the gifs to go to 
+          var gifDiv = $("<div>");
+          gifDiv.addClass("thingsILike col-xs-12 col-sm-6 col-md-3 col-lg-2")
+          //shows the gifs rating and creates a p tag for it
+          var rating = results[i].rating;
+          var p = $("<p>").text("Rating: " + rating);
+          // creates a place for the gifimage to go
+          var gifImageActive = $("<img>");
+          var gifImageStill = $("<img>");
+          gifImageStill.attr("src", results[i].images.fixed_width_still.url);
+          gifImageStill.addClass("still");
+          gifImageActive.attr("src", results[i].images.fixed_width.url);
+          gifImageActive.addClass("active-gif hidden")
+          gifDiv.append(p);
+          gifDiv.append(gifImageStill);
+          gifDiv.append(gifImageActive);
+          $("#gifArea").prepend(gifDiv);
+        }
+      };
+    });
+};
+var toggleGIFs = function () {
+  $(this).children("img").toggleClass("hidden")
+};
+
+$(document).ready(function () {
+  renderButtons();
+});
 
 //function for adding new buttons from the user input
 $("#add-gifs").on("click", function (event) {
@@ -38,46 +87,16 @@ $("#add-gifs").on("click", function (event) {
   $("#topic-input").val("");
 });
 
-function displayGIFS() {
-  var topicGIF = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    topicGIF + "&api_key=" + APIkey + "&limit=10";
-  $("#gifArea").empty();
-  //ajax call for button pressed
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    .then(function (response) {
-      console.log(response)
-      var results = response.data;
-      for (var i = 0; i < results.length; i++) {
-        // keeps this SFW
-        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-          //creates new div for the gifs to go to 
-          var gifDiv = $("<div>");
-          //shows the gifs rating and creates a p tag for it
-          var rating = results[i].rating;
-          var p = $("<p>").text("Rating: " + rating);
-          // creates a place for the gifimage to go
-          var gifImageStill = $("<img>");
-          gifImageStill.attr("src", results[i].images.original_still.url);
-          gifDiv.append(p);
-          gifDiv.append(gifImageStill);
-          $("#gifArea").prepend(gifDiv);
-        }
-      }
-    });
-};
-function clickToPlay() {
-  var state = $(this).attr("src", results[i].images.original_still.url);
-  console.log(state)
-}
+
+
+
+
+
 $(document).on("click", ".topic-btn", displayGIFS)
+$(document).on("click", ".thingsILike", toggleGIFs)
 
 
-//calls the renderButtons function to display initial topic array
-renderButtons();
+
 
 
 
