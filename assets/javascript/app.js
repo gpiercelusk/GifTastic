9fd13be1ceee484856242d0d6873a7ca286c7fc6
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-03-14 21:19:53 -0400

    styling still in progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 //calls the renderButtons function to display initial topic array
 
 
-var topics = ["Alpacas", "Cooking", "Baking", "Arctic Monkyes", "The Beatles", "One Piece", "Dethklok", "Chocolate", "Schnauzer", "Vinyl", "Primus", "Ska"];
+var topics = ["Board Games", "Cooking", "Baking", "Arctic Monkyes", "The Beatles", "One Piece", "Dethklok", "Chocolate", "Schnauzers", "Vinyl", "Primus", "Ska"];
 
 
 var APIkey = "bSjgDH5z6GZLhVQNHZLRlLNvnOhhG65r";
@@ -17,10 +17,10 @@ function renderButtons() {
     //sets "a" to generate a button
     var a = $("<button>");
     //adds a class of topic-btn to the button
-    a.addClass("topic-btn btn btn-warning col-xs-12 col-sm-6 col-md-3 col-lg-2");
+    a.addClass("topic-btn btn btn-warning col-sm-12 col-md-12 col-lg-2");
     //adds attribute to the buttons
     a.attr("data-name", topics[i]);
-    //adds text to the button 
+    //adds text to the button
     a.text(topics[i]);
     //appends to buttons-view div
     $("#buttons-view").append(a);
@@ -46,7 +46,7 @@ function displayGIFS() {
         if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
           //creates new div for the gifs to go to 
           var gifDiv = $("<div>");
-          gifDiv.addClass("thingsILike col-xs-12 col-sm-6 col-md-3 col-lg-2")
+          gifDiv.addClass("thingsILike col-xs-12 col-sm-6 col-md-6 col-lg-3")
           //shows the gifs rating and creates a p tag for it
           var rating = results[i].rating;
           var p = $("<p>").text("Rating: " + rating);
@@ -56,11 +56,13 @@ function displayGIFS() {
           gifImageStill.attr("src", results[i].images.fixed_width_still.url);
           gifImageStill.addClass("still");
           gifImageActive.attr("src", results[i].images.fixed_width.url);
-          gifImageActive.addClass("active-gif hidden")
-          gifDiv.append(p);
-          gifDiv.append(gifImageStill);
-          gifDiv.append(gifImageActive);
+          gifImageActive.addClass("active-gif hidden");
+
+
+          gifDiv.prepend(gifImageStill);
+          gifDiv.prepend(gifImageActive);
           $("#gifArea").prepend(gifDiv);
+          gifDiv.append(p);
         }
       };
     });
@@ -100,6 +102,5 @@ $(document).on("click", ".thingsILike", toggleGIFs)
 
 
 
-// // Adding a click event listener to all elements with a class of "movie-btn"
-// $(document).on("click", ".movie-btn", displayMovieInfo);
+
 
